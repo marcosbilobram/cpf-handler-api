5a2dd8c337152f22f3d4ce1e437062666efb83e8
Marcos Bilobram <marcosbilobram@hotmail.com>
2023-03-22 10:40:57 -0300

    initial commit

---
+++ src/main/java/com/max/cpfhandler/cpfHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.max.cpfhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class cpfHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(cpfHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/max/cpfhandler/cpfHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.cpfhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class cpfHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
